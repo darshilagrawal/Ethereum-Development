a25c74d1db4e9bae5ee7531c650497d45b9fc5cf
darshilagrawal <darshilagrawal2000@gmail.com>
2021-05-31 00:54:47 +0530

    Deploying Smart Contract on Local Test Network and creating its local instance.

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const ganache=require('ganache-cli');
 const Web3 = require('web3');
 const web3=new Web3(ganache.provider());
+const {interface,bytecode} = require('../compile')
 
 // class Car{
 //   park(){
@@ -25,18 +26,21 @@ const web3=new Web3(ganache.provider());
 //     assert.equal(car.drive(),'vroom')
 //   });
 // })
-
-beforeEach(() => {
+let accounts;
+let inbox;
+beforeEach(async () => {
   // Get a list of all the unlocked Accounts
-  web3.eth.getAccounts()
-    .then(fetchedAccounts => {
-      console.log(fetchedAccounts);
-    });
+  accounts = await web3.eth.getAccounts();
 
   //Use one of those accounts to deploy
   //The Contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  .deploy({ data:bytecode,arguments:['Hi there!'] })
+  .send({from: accounts[0],gas:'1000000' });
 });
 
-describe('Inboox',() =>{
-  it('Deploys a Contract',() => {});
+describe('Inbox',() =>{
+  it('Deploys a Contract',() => {
+    console.log(inbox);
+  });
 });
