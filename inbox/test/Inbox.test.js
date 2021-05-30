c17726bbb78a0f97c491901beb78e75a535f3b5b
darshilagrawal <darshilagrawal2000@gmail.com>
2021-05-30 23:59:08 +0530

    Generating Unlocked Ethereum Accounts for testing using Ganache

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,25 +3,40 @@ const ganache=require('ganache-cli');
 const Web3 = require('web3');
 const web3=new Web3(ganache.provider());
 
-class Car{
-  park(){
-    return 'stopped'
-  }
-  drive(){
-    return 'vroom'
-  }
-}
+// class Car{
+//   park(){
+//     return 'stopped'
+//   }
+//   drive(){
+//     return 'vroom'
+//   }
+// }
+//
+// let car;
+// beforeEach(() => {
+//   car=new Car;
+// })
+// describe('Car',() => {
+//   it('parking enabled', () => {
+//   assert.equal(car.park(), 'stopped');
+//   });
+//
+//   it('Driving Functionality',() => {
+//     assert.equal(car.drive(),'vroom')
+//   });
+// })
 
-let car;
 beforeEach(() => {
-  car=new Car;
-})
-describe('Car',() => {
-  it('parking enabled', () => {
-  assert.equal(car.park(), 'stopped');
-  });
+  // Get a list of all the unlocked Accounts
+  web3.eth.getAccounts()
+    .then(fetchedAccounts => {
+      console.log(fetchedAccounts);
+    });
 
-  it('Driving Functionality',() => {
-    assert.equal(car.drive(),'vroom')
-  });
-})
+  //Use one of those accounts to deploy
+  //The Contract
+});
+
+describe('Inboox',() =>{
+  it('Deploys a Contract',() => {});
+});
