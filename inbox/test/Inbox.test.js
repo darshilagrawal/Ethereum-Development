7ad2cd055e17f622c331416cab59712d0002d4cc
darshilagrawal <darshilagrawal2000@gmail.com>
2021-06-01 23:39:22 +0530

    Added Custom Test set for Checking Deployment ,Initial Value and changed value of inbox Storage container

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert');
 const ganache=require('ganache-cli');
 const Web3 = require('web3');
-const web3=new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const {interface,bytecode} = require('../compile')
 
 // class Car{
@@ -28,6 +29,7 @@ const {interface,bytecode} = require('../compile')
 // })
 let accounts;
 let inbox;
+
 beforeEach(async () => {
   // Get a list of all the unlocked Accounts
   accounts = await web3.eth.getAccounts();
@@ -37,10 +39,21 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
   .deploy({ data:bytecode,arguments:['Hi there!'] })
   .send({from: accounts[0],gas:'1000000' });
+  inbox.setProvider(provider);
+
 });
 
 describe('Inbox',() =>{
   it('Deploys a Contract',() => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+  it('Default Message Present', async () =>{
+    const message = await inbox.methods.message().call();
+    assert.equal(message,'Hi there!');
+  });
+  it('can Change Message', async () => {
+    await inbox.methods.setMessage('Nope').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message,'Nope');
   });
 });
